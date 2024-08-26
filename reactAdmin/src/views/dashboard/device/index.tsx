34d7f902705f358752747b3c34700665327978ea
raopan2021 <raopan2021@outlook.com>
2024-08-26 22:59:24 +0800

    内存chart调整

--- reactAdmin/src/views/dashboard/device/index.tsx
+++ reactAdmin/src/views/dashboard/device/index.tsx
@@ -1,37 +1,39 @@
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import api from '../../../api';
 import './index.scss';
 import Memory from './memory';
 
 const Device: React.FC = () => {
-    const theme = useSelector(state => state.theme.value)
-
-    const [memoryData, setMemoryData] = useState({})
+    const [memoryData, setMemoryData] = useState({});
 
     //    每秒发送一次请求
     useEffect(() => {
         const timer = setInterval(() => {
-            getData()
-        }, 1000)
+            getData();
+        }, 1000);
         return () => {
-            clearInterval(timer)
-        }
-    })
+            clearInterval(timer);
+        };
+    });
 
     const getData = () => {
         api.get('device')
             .then(res => {
-                setMemoryData(res.map(({ totalmem, freemem, create_time }) => {
+                const chart = res.map(({ totalmem, freemem, create_time }) => {
                     return {
                         create_time: dayjs(create_time).format('mm:ss'),
-                        value: totalmem - freemem
-                    }
-                }))
-                console.log(memoryData);
-            })
-    }
+                        value: totalmem - freemem,
+                    };
+                });
+                const { totalmem, freemem } = res[res.length - 1];
+                setMemoryData({
+                    chart,
+                    totalmem,
+                    freemem,
+                });
+            });
+    };
 
     return (
         <>
